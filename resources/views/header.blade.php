5669c4044ea9cba0ec98eafff6a558c0fe18393b
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-07-04 20:39:22 +0200

    1. Creazione repo

---
+++ resources/views/header.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Boolean_AR</title>
+    <link rel="stylesheet" href="public/css/app.css">
+  </head>
+  <body>
+    <header>
+        <div class="header-left">
+            <img src="img/logo.png" alt="">
+        </div>
+        <div class="header-right">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Scopri il Corso</a>
+                </li>
+                <li>
+                    <a href="#">La nostra storia</a>
+                </li>
+                <li>
+                    <a href="#" class="cta">Iscriviti</a>
+                </li>
+            </ul>
+            <a href="#">
+                <i class="fas fa-bars"></i>
+            </a>
+        </div>
+        <div class="hamburger-menu">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Scopri il Corso</a>
+                </li>
+                <li>
+                    <a href="#">La nostra storia</a>
+                </li>
+                <li>
+                    <a href="#" class="cta">Iscriviti</a>
+                </li>
+            </ul>
+            <a href="#" class="close">
+                <i class="fas fa-times"></i>
+            </a>
+        </div>
+    </header>
+  </body>
+</html>
